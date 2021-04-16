8dac2b9911866fcfc0ec647c4ec43a67f912f2e3
Karan Gandhi <kag354@rutgers.edu>
2021-04-16 11:36:09 -0400

    Initial Commit

---
+++ M/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  M
+//
+//  Created by Karan Gandhi on 4/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        outputLabel.text = "HI"
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    
+    
+    
+  
+}
+
