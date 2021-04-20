a963ac2e839068e8b24b010dd2d69a4fb73b1a35
Karan Gandhi <kag354@rutgers.edu>
2021-04-20 11:46:17 -0400

    Add function

--- M/ViewController.swift
+++ M/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        outputLabel.text = "Helloo"
+        outputLabel.text = "Add this!"
         
         
         // Do any additional setup after loading the view.
